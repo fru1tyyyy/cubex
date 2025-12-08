f2f45606c0f40cde6f68aecc8ae8399c95a6e688
Been <161136564+fru1tyyyy@users.noreply.github.com>
2025-12-09 00:16:43 +0800

    Update Register.ts

--- backend/routes/Register.ts
+++ backend/routes/Register.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { db } from "../connect";
 import { RowDataPacket } from "mysql2";
+import bcrypt from "bcryptjs";
 
 const router = Router();
 
@@ -12,8 +13,8 @@ router.post("/", (req, res) => {
     }
 
     const checkEmail = "SELECT * FROM user WHERE email = ?";
-    db.query<RowDataPacket[]>(checkEmail, [email], (err, results) => {
-        if (err) {
+    db.query<RowDataPacket[]>(checkEmail, [email], async (err, results) => {
+        if (err){
             console.error("Database error:", err);
             return res.status(500).json({ message: "Database error" });
         }
@@ -22,14 +23,21 @@ router.post("/", (req, res) => {
             return res.status(400).json({ message: "Email already exists" });
         }
 
-        const sql = "INSERT INTO user (name, email, password) VALUES (?, ?, ?)";
-        db.query(sql, [name, email, password], (err2) => {
-            if (err2) {
-                console.error("Insert error:", err2);
-                return res.status(500).json({ message: "Database insert error" });
-            }
-            res.json({ message: "User registered successfully!" });
-        });
+        try{
+          const hashedPassword = await bcrypt.hash(password, 10);
+
+          const sql = "INSERT INTO user (name, email, password) VALUES (?, ?, ?)";
+          db.query(sql, [name, email, hashedPassword], (err2) => {
+              if (err2) {
+                  console.error("Insert error:", err2);
+                  return res.status(500).json({ message: "Database insert error" });
+              }
+              res.json({ message: "User registered successfully!" });
+            });
+        } catch (error) {
+            console.error("Hash error:", error);
+            res.status(500).json({ message: "Server error" });
+        }
     });
 });
 
