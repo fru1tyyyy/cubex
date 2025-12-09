6322b37734c4cfcf499ab3b2af2f41bbd2cadb90
Been <161136564+fru1tyyyy@users.noreply.github.com>
2025-12-09 08:32:58 +0800

    Add files via upload

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import "../assets/styles/footer.css";
+
+export default function Footer(){
+    return(
+        <footer className="footer-container">
+      <p>© 2025 CubeX</p>
+    </footer>
+    )
+}
